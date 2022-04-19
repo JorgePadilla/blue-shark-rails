e0eb3f1b0f2851925cb5476054f8bbbffcc19c09
JorgePadilla <jorgep4dill4@gmail.com>
2022-04-18 20:32:18 -0600

    registre fix

--- app/controllers/user_registration_controller.rb
+++ app/controllers/user_registration_controller.rb
@@ -1,11 +1,13 @@
 class UserRegistrationController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
+
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
-    @user.profile = Patient.new
+    @user.profile = Patient.new(email: user_params[:email])
     if @user.save
       auto_login(@user)
       redirect_to dashboard_path, notice: "You successfully register"
