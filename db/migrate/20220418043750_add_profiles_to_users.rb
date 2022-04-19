d2ab2a8d5756cc8314a49eecb6fe7e91b99b5335
JorgePadilla <jorgep4dill4@gmail.com>
2022-04-18 21:26:28 -0600

    registre fix2

--- db/migrate/20220418043750_add_profiles_to_users.rb
+++ db/migrate/20220418043750_add_profiles_to_users.rb
@@ -1,5 +1,5 @@
 class AddProfilesToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_reference :users, :profile, polymorphic: true, null: false
+    add_reference :user, :profile, polymorphic: true, null: false
   end
 end
