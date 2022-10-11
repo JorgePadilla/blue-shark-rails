6bac45bf8047039b8579b095c169d55381170f61
JorgePadilla <jorgep4dill4@gmail.com>
2022-10-10 22:21:02 -0400

    add fullcalendar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,21 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+document.addEventListener("DOMContentLoaded", function() {
+    var calendarEl = document.getElementById('calendar');
+  
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        initialDate: '2022-02-01',
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        events: '/mc',
+        eventColor: '#378006'
+      });
+    
+      calendar.render();
+});
